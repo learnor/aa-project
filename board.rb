0df79c4a0c5b4d366d7458ff36f75395af5dfde0
Zion <learnor@gmail.com>
2017-12-07 16:38:10 -0800

    Remove extra end in Board#solved? method

--- board.rb
+++ board.rb
@@ -54,8 +54,7 @@ class Board
   def solved?
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
-      squares.all? { |square| solved_set?(square) }
-    end
+      squares.all? { |square| solved_set?(square) } 
   end
 
   def solved_set?(tiles)
